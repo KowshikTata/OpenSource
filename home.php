a88f3be0ae59278365c315116a2cede0b73b7e23
Tata Kowshik <32011616+KowshikTata@users.noreply.github.com>
2020-04-11 17:50:32 +0530

    Home.php
    Home

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+.w3-btn,.w3-button{border:none;display:inline-block;padding:8px 16px;vertical-align:middle;overflow:hidden;text-decoration:none;color:inherit;background-color:inherit;text-align:center;cursor:pointer;white-space:nowrap}
+.w3-bar{width:100%;overflow:hidden}.w3-center .w3-bar{display:inline-block;width:auto}
+.w3-bar .w3-bar-item{padding:8px 16px;float:left;width:auto;border:none;display:block;outline:0}
+.w3-bar .w3-dropdown-hover,.w3-bar .w3-dropdown-click{position:static;float:left}
+.w3-bar .w3-button{white-space:normal}
+.w3-bar-block .w3-bar-item{width:100%;display:block;padding:8px 16px;text-align:left;border:none;white-space:normal;float:none;outline:0}
+.w3-bar-block.w3-center .w3-bar-item{text-align:center}.w3-block{display:block;width:100%}
+.w3-black,.w3-hover-black:hover{color:#fff!important;background-color:#000!important}
+.w3-bar .w3-button{white-space:normal}
+.w3-black,.w3-hover-black:hover{color:#fff!important;background-color:#000!important}
+</style>
+
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="w3-bar w3-black">
+  <a href="home.php" class="w3-button w3-bar-item">Home</a>
+  <a href="product.php" class="w3-button w3-bar-item">Product</a>
+  <a href="aboutUs.php" class="w3-button w3-bar-item">About Us</a>
+  <a href="forms.php" class="w3-button w3-bar-item">Sign Up</a>
+</nav>
+<marquee direction="slide" >
+<img src="smartbuylogo.jpg" width="580px">
+<img src="pic1.jpg" width="580px">
+
+</marquee>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+<center> 
+ <a href="#"><i class="fa fa-facebook-official"></i></a>
+  <a href="#"><i class="fa fa-pinterest-p"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-flickr"></i></a>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <p>&copy; SmartBuy</p>
+</center>
+</footer>
+
+</body>
+</html>
